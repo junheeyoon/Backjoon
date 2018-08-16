0b303004ea8587a2d8d0587d8364ce15e18d3daa
junheeyoon <enteryjh@ajou.ac.kr>
2018-08-16 23:04:51 +0900

    7562

--- BFS/7562/7562.cpp
+++ BFS/7562/7562.cpp
@@ -2,40 +2,59 @@
 #include <queue>
 using namespace std;
 queue<pair<int, int> > q;
-int arr[101][101];
-int visit[101][101];
-int N;
-int result_x, result_y;
-int num;
-int x, y;
 
-void calculate(){
+int arr[301][301];
+int visit[301][301];
+int N;
+int array_x[8] = {-2, -2, -1, -1, 1, 1, 2, 2};
+int array_y[8] = {-1, 1, -2, 2, 2, -2, 1, -1};
+int next_x, next_y;
+int t;
+int start_x, start_y;
+int end_x, end_y;
+
+void night(int x, int y){
+	if(x < 0 || y < 0 || x >= N || y >= N){
+			return;
+	}
 	while(!q.empty()){
-		printf("2\n");
-		for(int i = 0; i < N; i++){
-			if(arr[q.front().second][i] && !visit[q.front().first][i]){
-				q.push(make_pair(q.front().first, i));
-				visit[q.front().first][i] = visit[q.front().first][q.front().second] + 1;
+		for(int i = 0; i < 8; i++){
+			next_x = x + array_x[i];
+			next_y = y + array_y[i];
+			
+			if(!visit[next_x][next_y] && next_x >= 0 && next_y >= 0 && next_x < N && next_y < N){
+
+				visit[next_x][next_y] = visit[x][y] + 1;
+				q.push(make_pair(next_x, next_y));			
 			}
 		}
-		printf("%d %d\n", q.front().first, q.front().second);
 		q.pop();
+		night(q.front().first, q.front().second);
 	}
 }
 
 int main(){
-	printf("hello");
-	scanf("%d %d %d %d", &N, &result_x, &result_y, &num);
-	printf("%d", num);
-	while(num--){
-		scanf("%d %d", &x, &y);
-		printf("tt\n");
-		arr[x][y] = 1;
-		q.push(make_pair(x, y));
-	}
-	printf("1\n");
-	calculate();
 	
-	printf("%d\n", visit[result_x][result_y]);
-
+	scanf("%d", &t);
+	while(t--){
+		for(int m = 0; m < N; m++){
+			for(int n = 0; n < N; n++){
+				arr[m][n] = 0;
+				visit[m][n] = 0;
+			}
+		}
+		scanf("%d %d %d %d %d", &N, &start_x, &start_y, &end_x, &end_y);
+		arr[start_x][start_y] = 1;
+		for(int i = 0; i < N; i++){
+			for(int j = 0; j < N; j++){
+				if(arr[i][j] && !visit[i][j]){
+					visit[i][j] = 1;
+					q.push(make_pair(i, j));
+					night(i, j);
+				}
+			}
+		}
+		printf("%d\n", visit[end_x][end_y] - 1);
+	}
+	
 }
